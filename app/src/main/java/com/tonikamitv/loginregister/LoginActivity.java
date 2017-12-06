8471553557e1ab34d4689394030e21c9ce71ee17
Bhavan Pandya <pandyabhavan134@gmail.com>
2017-12-06 12:16:13 -0800

    pROJECT

---
+++ app/src/main/java/com/tonikamitv/loginregister/LoginActivity.java
@@ -0,0 +1,106 @@
+package com.tonikamitv.loginregister;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final TextView tvRegisterLink = (TextView) findViewById(R.id.tvRegisterLink);
+        final Button bLogin = (Button) findViewById(R.id.bSignIn);
+
+        tvRegisterLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+                Log.d(username,password);
+                /*LoginRequest loginRequest = new LoginRequest(json, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);*/
+                sendLoginRequest(username,password);
+            }
+        });
+    }
+
+    /**
+     * Method to make json object request where json response starts wtih {
+     * */
+    private void sendLoginRequest(String username,String password) {
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("email", username);
+            json.put("password", password);
+        }catch (Exception e){}
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                "https://library-system-backend.herokuapp.com/users/login", json, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                //Log.d("", response.toString());
+
+                try {
+                    Log.d("",response.toString());
+                    Toast.makeText(getApplicationContext(),
+                            response.get("status").toString(),
+                            Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("Error", "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        "Error", Toast.LENGTH_SHORT).show();
+                // hide the progress dialog
+            }
+        });
+
+        // Adding request to request queue
+        RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+        queue.add(jsonObjReq);
+    }
+}
